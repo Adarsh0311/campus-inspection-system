c7f23c4670fe530367577ca1bb234a30ce0c615c
adarsh0311 <adarshghai6@gmail.com>
2025-10-06 00:44:39 -0500

    feat: set up backend with PostgreSQL connection and environment config

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const { pool } = require('./connection');
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+//db connection
+pool.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => console.error('Database connection error', err.stack));
+
+// test route
+app.get('/', (req, res) => {
+  res.send('Campus Inspection System API is running...');
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
