2d67dcd3a589eea0a75ddf565ac38079cdd153ee
adarsh0311 <adarshghai6@gmail.com>
2025-10-17 02:03:33 -0500

    feat: add test user creation functionality with email and password hashing

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,10 @@ const { authMiddleware, adminMiddleware } = require('./middleware/authMiddleware
 
 const cors = require('cors');
 
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
+const prisma = new PrismaClient();
+
 //cors
 app.use(cors());
 
@@ -33,6 +37,47 @@ app.use(authMiddleware);
 app.use('/api/users', userRoute);
 app.use('/api/buildings', adminMiddleware, buildingRoute);
 
+
+
+//create a test user function
+async function testUser() {
+
+    const email = 'admin@test.com';
+
+    const existsByEmail = async () => {
+        const user = await prisma.user.findUnique({
+            where: { email: email },
+        });
+        return !!user;
+    };
+
+
+
+    if (await existsByEmail()) {
+        console.log('Test user already exists');
+        return;
+    }
+
+    try {
+        await prisma.user.create({
+            data: {
+                email: email,
+                password: await bcrypt.hash('1234', 10),
+                firstName: 'Test',
+                lastName: 'user',
+                role: 'ADMIN'
+            }
+        });
+       console.log('Test user created with email: ' + email + ' and password: 1234');
+    } catch (error) {
+        console.log('Error creating test user: ' + error.message);
+    }
+}
+
+
+
 app.listen(PORT, () => {
     console.log('Server is running on port: ' + PORT);
+    testUser();
+
 });
